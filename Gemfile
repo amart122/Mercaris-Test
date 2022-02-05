ced23b30bbd26a39feb6d5a1cb3fa3e0067426ee
Aram Martinez <amart122@asu.edu>
2022-02-05 15:35:40 -0800

    Add Chartkick gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.5.5'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 
 gem 'mysql2'
+gem 'chartkick'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import Chart from 'chart.js/auto';
+import 'chartkick/chart.js'
 
-global.Chart = Chart
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
