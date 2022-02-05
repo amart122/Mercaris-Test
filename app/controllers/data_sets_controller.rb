ea8650c952c8329818dea15cab2ed72c5cf54018
Aram Martinez <amart122@asu.edu>
2022-02-05 15:33:32 -0800

    Parse CSV file and save rows as DataEntries

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -6,4 +6,21 @@ class DataSetsController < ApplicationController
     def new
         
     end
+
+    def create
+        redirect_back(fallback_location: new_data_set_path) if data_set_params[:data_set_file].blank?
+        new_set = DataSet.create(data_set_params)
+        if new_set.present?
+            redirect_to data_set_path(new_set.id)
+        else
+            redirect_back(fallback_location: new_data_set_path)
+        end
+    end
+
+
+    private
+
+    def data_set_params
+        params.require(:data_set).permit(:name, :data_set_file)
+    end
 end
--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -1,3 +1,21 @@
 class DataSet < ApplicationRecord
     has_many :data_entries
+    attr_accessor :data_set_file
+    after_create :import_data_set
+
+    require 'csv'
+
+    private
+    def import_data_set
+        CSV.foreach(self.data_set_file.path, headers: true, skip_blanks: true) do |row|
+            next if row.to_hash.values.all?(&:blank?)
+            DataEntry.create(
+                contract: row[0],
+                transaction_date: Date.strptime(row[1], '%m/%d/%y'),
+                quantity: row[2].to_f.round(6),
+                price: row[3].to_f,
+                data_set_id: self.id
+            )
+        end
+    end
 end
